d6aa705e60ce20ba33e2b9b83519e50595905623
Dirk Drutschmann <dirkdrutschmann@gmail.com>
2021-10-20 12:04:47 +0200

    initial Pac-Bomb

--- src/main/java/de/bhtpaf/pacbomb/SnakeTest.java
+++ src/main/java/de/bhtpaf/pacbomb/SnakeTest.java
@@ -1,65 +1,58 @@
 package de.bhtpaf.pacbomb;
 
-import javafx.application.Application;
-import javafx.stage.Stage;
-
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
-
-
 import javafx.animation.AnimationTimer;
-
+import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
+import javafx.stage.Stage;
 
 
-public class Snake extends Application {
+import java.util.Random;
+
+
+public class SnakeTest extends Application  {
     // variable
-    static int speed = 5;
+    static int speed = 10;
+    static int bombs = -1;
     static int foodcolor = 0;
-    static int width = 20;
-    static int height = 20;
+    static int width = 500;
+    static int height = 500;
     static int foodX = 0;
     static int foodY = 0;
     static int cornersize = 25;
-    static List<Corner> snake = new ArrayList<>();
-    static Dir direction = Dir.left;
+    static int step = 2;
+    static Dir direction = Dir.right;
     static boolean gameOver = false;
     static Random rand = new Random();
-
-
+    //Creating an image
+    static Image image = new Image(SnakeTest.class.getResourceAsStream("bomberman.gif"));
+    //Upper Left of Image / Image is set 50x50
+    static int PacBombX = width / 2 ;
+    static int PacBombY = height /2 ;
 
     public enum Dir {
         left, right, up, down
     }
 
-    public static class Corner {
-        int x;
-        int y;
 
-        public Corner(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
 
-    }
-
-    public void start(Stage primaryStage) {
+    public void start(Stage primaryStage)  {
         try {
             newFood();
 
             VBox root = new VBox();
-            Canvas c = new Canvas(width * cornersize, height * cornersize);
+            Canvas c = new Canvas(width , height );
             GraphicsContext gc = c.getGraphicsContext2D();
             root.getChildren().add(c);
+
+
 
             new AnimationTimer() {
                 long lastTick = 0;
@@ -79,7 +72,7 @@ public class Snake extends Application {
 
             }.start();
 
-            Scene scene = new Scene(root, width * cornersize, height * cornersize);
+            Scene scene = new Scene(root, width , height);
 
             // control
             scene.addEventFilter(KeyEvent.KEY_PRESSED, key -> {
@@ -95,17 +88,13 @@ public class Snake extends Application {
                 if (key.getCode() == KeyCode.RIGHT) {
                     direction = Dir.right;
                 }
-
             });
 
-            // add start snake parts
-            snake.add(new Corner(width / 2, height / 2));
-            snake.add(new Corner(width / 2, height / 2));
-            snake.add(new Corner(width / 2, height / 2));
+
             //If you do not want to use css style, you can just delete the next line.
           //  scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
             primaryStage.setScene(scene);
-            primaryStage.setTitle("SNAKE GAME");
+            primaryStage.setTitle("Pac-Bomb");
             primaryStage.show();
         } catch (Exception e) {
             e.printStackTrace();
@@ -121,33 +110,32 @@ public class Snake extends Application {
             return;
         }
 
-        for (int i = snake.size() - 1; i >= 1; i--) {
-            snake.get(i).x = snake.get(i - 1).x;
-            snake.get(i).y = snake.get(i - 1).y;
-        }
+
+
+
 
         switch (direction) {
             case up:
-                snake.get(0).y--;
-                if (snake.get(0).y < 0) {
+                PacBombY -= step;
+                if (PacBombY < 0) {
                     gameOver = true;
                 }
                 break;
             case down:
-                snake.get(0).y++;
-                if (snake.get(0).y > height) {
+                PacBombY += step;
+                if (PacBombY + 50 > height) {
                     gameOver = true;
                 }
                 break;
             case left:
-                snake.get(0).x--;
-                if (snake.get(0).x < 0) {
+                PacBombX -= step;
+                if (PacBombX < 0) {
                     gameOver = true;
                 }
                 break;
             case right:
-                snake.get(0).x++;
-                if (snake.get(0).x > width) {
+                PacBombX += step;
+                if (PacBombX + 50 > width) {
                     gameOver = true;
                 }
                 break;
@@ -155,27 +143,21 @@ public class Snake extends Application {
         }
 
         // eat food
-        if (foodX == snake.get(0).x && foodY == snake.get(0).y) {
-            snake.add(new Corner(-1, -1));
+        if (foodX >= PacBombX && foodX <= PacBombX+50 && foodY >= PacBombY && foodY <= PacBombY+50) {
             newFood();
         }
 
-        // self destroy
-        for (int i = 1; i < snake.size(); i++) {
-            if (snake.get(0).x == snake.get(i).x && snake.get(0).y == snake.get(i).y) {
-                gameOver = true;
-            }
-        }
+
 
         // fill
         // background
         gc.setFill(Color.BLACK);
-        gc.fillRect(0, 0, width * cornersize, height * cornersize);
+        gc.fillRect(0, 0, width , height );
 
         // score
         gc.setFill(Color.WHITE);
         gc.setFont(new Font("", 30));
-        gc.fillText("Score: " + (speed - 6), 10, 30);
+        gc.fillText("BOMBS: " + bombs, 10, 30);
 
         // random foodcolor
         Color cc = Color.WHITE;
@@ -198,30 +180,22 @@ public class Snake extends Application {
                 break;
         }
         gc.setFill(cc);
-        gc.fillOval(foodX * cornersize, foodY * cornersize, cornersize, cornersize);
+        gc.fillOval(foodX , foodY , cornersize, cornersize);
 
-        // snake
-        for (Corner c : snake) {
-            gc.setFill(Color.LIGHTGREEN);
-            gc.fillRect(c.x * cornersize, c.y * cornersize, cornersize - 1, cornersize - 1);
-            gc.setFill(Color.GREEN);
-            gc.fillRect(c.x * cornersize, c.y * cornersize, cornersize - 2, cornersize - 2);
 
-        }
+
+        //Setting the image view
+        gc.drawImage(image, PacBombX, PacBombY, 50, 50);
 
     }
 
     // food
     public static void newFood() {
         start: while (true) {
+            bombs++;
             foodX = rand.nextInt(width);
             foodY = rand.nextInt(height);
 
-            for (Corner c : snake) {
-                if (c.x == foodX && c.y == foodY) {
-                    continue start;
-                }
-            }
             foodcolor = rand.nextInt(5);
             speed++;
             break;
